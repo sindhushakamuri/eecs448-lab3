a7eb0814cc30e6b3cde1ad8f438d86e9af89d92b
Sindhu Shakamuri <sindhu.shakamuri@gmail.com>
2019-10-01 17:33:37 -0500

    index file created

---
+++ exerciseTwo/slideshow.js
@@ -0,0 +1,22 @@
+let imgArr = ["pic1.jpg","pic2.jpg","pic3.jpg","pic4.jpg","pic5.jpg"];
+let imgArrLength = imgArr.length;
+let count = 1;
+function Previous()
+{
+  count = count -1;
+  if(count == 0)
+  {
+    count = imgArrLength;
+  }
+  document.getElementById("Move").src = imgArr[count-1];
+}
+
+function Next()
+{
+  count = count +1;
+  if(count == imgArrLength)
+  {
+    count = 1;
+  }
+  document.getElementById("Move").src = imgArr[count-1];
+}
